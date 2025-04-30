c84ec76eee5909d28a7c79f43764ea6a26a86eab
jagv091205 <jagveerkaurkhalsa@gmail.com>
2025-04-30 14:28:07 +0530

    updates

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bhookie.pos',
+  appName: 'bhookie-pos',
+  webDir: 'build'
+};
+
+export default config;
