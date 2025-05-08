b8b0b1a51053c7a92abcdc430843134c174c1c57
ridszz <riddhisawant153@gmail.com>
2025-05-08 11:39:47 +0530

    layout fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,33 +26,33 @@ export default function Footer() {
       <div className="flex flex-wrap justify-start gap-1">
         <button
           onClick={handleRefresh}
-          className="bg-blue-600 text-white font-bold py-2 px-2 rounded shadow text-sm w-[120px]"
+          className="bg-blue-600 text-white font-bold py-2 px-2 rounded shadow text-sm w-[120px]  h-[80px]"
         >
           REFRESH<br />SCREEN
         </button>
 
         <button
         onClick={handleRecallClick}  // Updated handler
-        className="bg-blue-600 text-white font-bold py-2 px-2 rounded shadow text-sm w-[120px]"
+        className="bg-blue-600 text-white font-bold py-2 px-2 rounded shadow text-sm w-[120px] h-[80px]"
       >
         RECALL<br />ORDER
       </button>
 
         <button
           onClick={handleManagerClick}
-          className="bg-red-600 text-white font-bold py-2 px-2 rounded shadow text-sm w-[140px]"
+          className="bg-red-600 text-white font-bold py-2 px-2 rounded shadow text-sm w-[140px]  h-[80px]"
         >
           MANAGER<br />SCREEN
         </button>
  
         <button onClick={handleHelpClick}
-          className="bg-gray-600 text-white font-bold py-2 px-2 rounded shadow text-sm w-[90px]"
+          className="bg-gray-600 text-white font-bold py-2 px-2 rounded shadow text-sm w-[90px] h-[80px]"
         >
           HELP
         </button>
 
         <button onClick={handleReportClick}
-          className="bg-blue-600 text-white font-bold py-2 px-2 rounded shadow text-sm w-[120px]"
+          className="bg-blue-600 text-white font-bold py-2 px-2 rounded shadow text-sm w-[120px] h-[80px]"
         >
           REPORT
         </button>
